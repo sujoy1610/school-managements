02c7827dfbc5b1ee4671d538730b50c23534d22f
Sujoy Sarkar <srkarsujoy715@gmail.com>
2025-09-03 03:32:13 +0530

    add railway

--- lib/db.ts
+++ lib/db.ts
@@ -9,17 +9,18 @@ const connectDB = async (): Promise<mysql.Connection> => {
 
   try {
     connection = await mysql.createConnection({
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'Sujoy@2000',
-      database: process.env.DB_NAME || 'schoolsdb',
+      host: process.env.MYSQLHOST,
+      user: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
+      port: Number(process.env.MYSQLPORT),
       charset: 'utf8mb4',
     });
 
-    console.log('✅ Connected to MySQL Database (schoolsdb)');
+    console.log('✅ Connected to Railway MySQL Database');
     return connection;
   } catch (error) {
-    console.error(' Database connection failed:', error);
+    console.error('❌ Database connection failed:', error);
     throw error;
   }
 };
